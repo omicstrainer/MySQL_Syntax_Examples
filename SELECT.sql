aceb0e6739b9c8862671ea99262f501db1273178
drabhishekkumar <draabhishekkumar@gmail.com>
2020-12-22 06:24:17 +0530

    Update SELECT.sql

--- SELECT.sql
+++ SELECT.sql
@@ -1,6 +1,10 @@
 -- SELECT Statement
 -- Types of SELECT Syntax
-SELECT column_name(s) FROM table_name;
+
+-------------------------------------------------
+-------SELECT column_name FROM table_name;-------
+--------------------------------------------------
+SELECT column_name FROM table_name;
 
 -- SELECT a column from the table employee_info
 SELECT Name FROM employee_info;

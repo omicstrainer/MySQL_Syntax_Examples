2486ef4dea1d1c1cbee94b9912a4e7dfd9c1e7b9
drabhishekkumar <draabhishekkumar@gmail.com>
2020-12-22 05:37:44 +0530

    Update SELECT.sql

--- SELECT.sql
+++ SELECT.sql
@@ -1,9 +1,10 @@
 -- SELECT Statement
 -- Types of SELECT Syntax
-SELECT column_name(s)
-FROM table_name;
+SELECT column_name(s) FROM table_name;
 
 -- SELECT a Column from the table employee_info
 
-SELECT Name
-FROM employee_info;
+SELECT Name FROM employee_info;
+
+
+SELECT GENE_ID  FROM rnaseq;

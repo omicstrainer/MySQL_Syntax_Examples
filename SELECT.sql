ebc78ce880fc16da19bd906728e5aaed84c70a0b
drabhishekkumar <draabhishekkumar@gmail.com>
2020-12-22 05:45:31 +0530

    Update SELECT.sql

--- SELECT.sql
+++ SELECT.sql
@@ -2,9 +2,32 @@
 -- Types of SELECT Syntax
 SELECT column_name(s) FROM table_name;
 
--- SELECT a Column from the table employee_info
-
+-- SELECT a column from the table employee_info
 SELECT Name FROM employee_info;
 
 
+-- SELECT the column GENE_ID  from the table rnaseq
 SELECT GENE_ID  FROM rnaseq;
+
+-- SELECT the column GENE_ID from the table rnaseq
+SELECT GENE_ID  FROM rnaseq;
+
+
+-- SELECT the column Gene_Annotation from the table rnaseq
+SELECT Gene_Annotation  FROM rnaseq;
+
+Expression_values
+-- SELECT the column Gene_Annotation from the table rnaseq
+SELECT Gene_Annotation  FROM rnaseq;
+Gene_length
+-- SELECT the column Gene_Annotation from the table rnaseq
+SELECT Gene_Annotation  FROM rnaseq;
+Unique_gene_reads
+-- SELECT the column Gene_Annotation from the table rnaseq
+SELECT Gene_Annotation  FROM rnaseq;
+
+-- SELECT the column Total_gene_read from the table rnaseq
+SELECT Total_gene_read  FROM rnaseq;
+
+-- SELECT the column RPKM from the table rnaseq
+SELECT RPKM  FROM rnaseq;
